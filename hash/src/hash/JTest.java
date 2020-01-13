7ab6e542154f2618c6e777f2fe48a7df26c60d25
1857364 <1857364@20.8.20.180>
2020-01-13 12:57:59 -0800

    more tests

--- hash/src/hash/JTest.java
+++ hash/src/hash/JTest.java
@@ -6,12 +6,19 @@ import org.junit.jupiter.api.Test;
 
 class JTest {
 	JUnit jUnit = new JUnit();
+	JUnit j2 = jUnit;
+	JUnit j3 = new JUnit();
 	@Test
 	void test() {
 		String expectedOutput = "loltyler1dotcom-discount-code-alpha";
 		String input = "loltyler1dotcom-discount-code-alpha";
 		
-		assertEquals(expectedOutput, jUnit.print(input));
+		assertEquals(expectedOutput, jUnit.print(input)); //pass
+		assertSame(jUnit, j2); //pass
+		assertSame(expectedOutput, jUnit.print(input));//pass
+		assertNotSame(jUnit, j3); 
+		assertNotEquals(jUnit.print(input), j2.print(input + "lol"));
+		//... other tests too 
 	}
 
 }
